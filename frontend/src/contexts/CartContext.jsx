d878eef205595bb5c7a0126e1926f70493fafe35
Rahmat Abdurrahman <yangmanaji@gmail.com>
2025-06-28 13:51:44 +0700

    cart ditambahkan

---
+++ frontend/src/contexts/CartContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+      setCartItems(JSON.parse(savedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const addToCart = (item) => {
+    const exists = cartItems.find(i => i.id === item.id);
+    if (exists) {
+      setCartItems(cartItems.map(i =>
+        i.id === item.id ? { ...i, quantity: i.quantity + 1 } : i
+      ));
+    } else {
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCartItems(cartItems.filter(i => i.id !== id));
+  };
+
+  const clearCart = () => setCartItems([]);
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
